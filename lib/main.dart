4dd94659ce294c2e0c7526d625a357aa402a6f94
mateusdiasherculano <mateusdias.her@gmail.com>
2025-04-18 21:38:15 -0300

    Criado o AppWidget com configuração de tema básico

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app/app_widget.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const AppWidget());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-    );
-  }
-}
-
